ea06098c39e4f1bad05781f772e90162704a4688
Muntean Gheorghe <gheorghe.muntean@ymail.com>
2011-10-13 19:07:15 +0300

    2 commit

--- lucrarea1.pl
+++ lucrarea1.pl
@@ -1,5 +1,30 @@
 use warnings;
 
+$a=12;
+print 'Valoarea lui $a ';
+
+$a= $a+2;
+print "este $a\n";
+print 'Linia 1\n$a\nLinia 2\n';
+print "Linia 1\n$a\nLinia 2\n";
+
+$nume='MUNTEAN';
+$prenume='Gigi';
+$salut="Hello Hello $prenume $nume";
+
+print "$salut";
+
+
+
+
+=pod
+print 255,"\n"; #zecimal
+print 0377,"\n"; #octal
+print 0b11111111,"\n"; #binar
+print 0xFF,"\n";
+
+
+
 @elevi=("Gigi","Carmen","Dan","Roxi");
 @colectie=("Gigi",9.7,"Carmen",8,"Dani","Roxi",'c');
 
@@ -10,17 +35,6 @@ print scalar(@elevi);
 print $#elevi;
 
 
-
-
-
-
-
-
-
-
-
-
-=pod
 $nume="petru";
 $adresa="paltinis, nr 4";
 $varsta=21;

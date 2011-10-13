cb570b51df8ab77dcdc93bfdb4cf561f800bb87b
Muntean Gheorghe <gheorghe.muntean@ymail.com>
2011-10-13 18:54:25 +0300

    1 commit

---
+++ lucrarea1.pl
@@ -0,0 +1,30 @@
+use warnings;
+
+@elevi=("Gigi","Carmen","Dan","Roxi");
+@colectie=("Gigi",9.7,"Carmen",8,"Dani","Roxi",'c');
+
+print $elevi[0];
+print $colectie[-1];
+
+print scalar(@elevi);
+print $#elevi;
+
+
+
+
+
+
+
+
+
+
+
+
+=pod
+$nume="petru";
+$adresa="paltinis, nr 4";
+$varsta=21;
+$medie=9.7;
+
+print "Nume: ",$nume,", Medie: ",$medie,", Adresa: ",$adresa,", Varsta: ",$varsta,"\n\n";
+=cut
